0fa352c5282351709297708f6126497a87fdf42b
TitasSp <titas.spegis@mif.stud.vu.lt>
2025-05-21 18:36:36 +0300

    funtionality prototypes

--- Vector.h
+++ Vector.h
@@ -1,35 +1,53 @@
 #pragma once
 #include <iostream>
+#include <stdexcept>
+#include <algorithm>
 
-class Vector{
+class Vector {
 private:
-  int cap; // talpa
-  int sz;        // elementų skaičius 
-  double *elem;  // rodyklė į elementus
+    int cap;        // capacity
+    int sz;         // number of elements
+    double* elem;   // pointer to elements
 
 public:
-  // konstruktoriai  
-  Vector();
-  Vector(int s);
-  Vector(int s, double val);
-  // set'eriai
-  void setElem (int id, double val);
-  // get'eriai
-  int size() const;
-  double getElem (int id);
-  double& operator[](int i);
-  const double& operator[](int i) const;
-  
-  // destruktorius: atlaisvina klasės objekto resursus
-  ~Vector();
+    // Constructors  
+    Vector();
+    Vector(int s);
+    Vector(int s, double val);
+    
+    Vector(const Vector& other);// Copy constructor
+    Vector(Vector&& other) noexcept;// Move constructor  
+    Vector& operator=(const Vector& other);// Copy assignment operator
+    Vector& operator=(Vector&& other) noexcept;// Move assignment operator
+    
+    // Setters
+    void setElem(int id, double val);
+    
+    // Getters
+    int size() const;
+    int capacity() const;
+    bool empty() const;
+    double& operator[](int i);
+    const double& operator[](int i) const;
+    double& at(int i);
+    const double& at(int i) const;
+    double& front();
+    const double& front() const;
+    double& back();
+    const double& back() const;
+    
+    // Destructor
+    ~Vector();
 
-  // funkcijos
-  void swap(int i, int j); // apkeičia du elementus vietomis
-  void erase(); // istrina vektorių
-  void insert(int i, double val); // įterpia elementą į vektorių
-  void pop(); // pašalina paskutinį elementą iš vektoriaus
-  void clear(); // išvalo vektorių elementu reiksmes
-  void resize(int s); // pakeičia vektoriaus dydį
-  void push_back(double val); // prideda elementą į vektoriaus pabaigą
-  void shrink_to_fit(); // sumažina vektoriaus dydį
+    // Functions
+    void swap(int i, int j);           // Swaps two elements
+    void erase();                      // Erases vector (sets size to 0)
+    void insert(int i, double val);    // Inserts element at position
+    void pop_back();                   // Removes last element
+    void clear();                      // Clears elements (sets to 0)
+    void resize(int s);                // Changes vector size
+    void resize(int s, double val);    // Changes vector size, fills with val
+    void reserve(int new_cap);         // Reserves capacity
+    void push_back(double val);        // Adds element to the end
+    void shrink_to_fit();              // Reduces capacity to fit size
 };
