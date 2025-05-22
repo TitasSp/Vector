a854129b528a5adebcd40ad4130bb845e5e9d02a
TitasSp <titas.spegis@mif.stud.vu.lt>
2025-05-22 14:52:05 +0300

    reallocs count

--- v1.5/vectors_testing/test.cpp
+++ v1.5/vectors_testing/test.cpp
@@ -2,12 +2,28 @@
 #include "../../Vector.h"
 #include "../mano_lib.h"
 
+template <typename V>
+int count_reallocations(V& vec, size_t N) {
+    size_t reallocs = 0;
+    size_t old_cap = vec.capacity();
+
+    for (size_t i = 0; i < N; ++i) {
+        vec.push_back(static_cast<int>(i));
+        if (vec.capacity() != old_cap) {
+            ++reallocs;
+            old_cap = vec.capacity();
+        }
+    }
+
+    return reallocs;
+}
+
 main(){
     vector<int> size = {10000, 100000, 1000000, 10000000, 100000000};
 
     for(int sz : size){
         cout << "Irasoma " << sz << " elementu" << endl;
-        std::vector<int> v1;
+        vector<int> v1;
         auto start = std::chrono::high_resolution_clock::now();
         for (int i = 1; i <= sz; ++i) v1.push_back(i);
         auto end = std::chrono::high_resolution_clock::now();
@@ -26,5 +42,17 @@ main(){
     
     }
 
+    const size_t N = 100000000;
+
+    vector<int> stdvec;
+    Vector<int> myvec;
+
+    int std_reallocs = count_reallocations(stdvec, N);
+    cout << "std::vector reallocations with 100000000: " << std_reallocs << "\n";
+
+    int my_reallocs = count_reallocations(myvec, N);
+    cout << "Vector reallocations with 100000000: " << my_reallocs << "\n";
+
+    return 0;
 }
  
